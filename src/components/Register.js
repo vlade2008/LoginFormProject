3462472a1e500a1f6e29fb16c9f4a813561f8942
vlade2008 <vlade1356@gmail.com>
2016-05-06 17:18:42 +0800

    intialize valadation form

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,19 +9,95 @@ import {Well,
 		Radio,
 		Checkbox} from 'react-bootstrap'
 
-export default  class Register extends React.Component{
+import validation from 'react-validation-mixin'
+import strategy from 'react-validatorjs-strategy'
+import validatorjs from 'validatorjs'
+import classnames from 'classnames'
+
+ class Register extends React.Component{
+
  constructor(props){
      super(props);
-
      this.state={
-
+     	
      };
 
+     this.validatorTypes = strategy.createSchema(
+	     	{
+	     		firstName:'required',
+	     		lastName:'required',
+	     		email:'required'	,
+	     		password:'required',
+	     		gender:'required',
+	     		food:'required'
+	     	},
+	     	{
+	     		"required":"The field: attribute is required!"
+	     	},
+	     	(validation)=>{
+	     		validation.setAttributeNames({
+	     			lastName:'lastName',
+	     			firstName:'firstName',
+	     			email:'email',
+	     			password:'password',
+	     			gender:'gender',
+	     			food:'food'
+	     		});
+	     	}
+     	);
 
  	}
 	 goToApp=()=>{
 	 	this.props.history.push("/");
 	 };
+
+	 getValidatorData = ()=> {
+        return this.state
+    };
+
+	getClasses = (field)=>{
+        return classnames({
+            'success': this.props.isValid(field),
+            'error': !this.props.isValid(field)
+        });
+ 	};
+
+
+	getErrorText=(field)=>{
+        var error = this.props.errors[field];
+        if(!error)
+            return null;
+        if(Array.isArray(error)){
+            var message = [];
+            message = error.map((item,i)=>{
+                return(
+                    <span key={i}>
+                        {item}
+                        <br/>
+                    </span>
+                )
+            });
+            return message;
+        }
+        else
+            return  (<span>{error || ''}</span>);
+    };
+
+    onFormSubmit = (event)=>{
+        event.preventDefault();
+        this.setState({
+        	validated:true
+        });
+        this.props.validate(this.onValidate);
+    };
+ 
+	onValidate=(error)=>{
+        if (error) {
+            //form has errors; do not submit
+        } else {
+           // submit to rest here
+        }
+    };
 
  render(){
  	const wellStyle={
@@ -42,10 +118,11 @@ export default  class Register extends React.Component{
 		<div className="container">
 		    <Well style={wellStyle}>
 		        <legend>Please Register</legend>
-		        { JSON.stringify(this.state)}
-			    <from>
-			    	<ControlLabel>First Name</ControlLabel>
+			    <form onSubmit={this.onFormSubmit} noValidate>
+			    	<FormGroup  validationState={this.getClasses('firstName')}>
+			    		<ControlLabel>First Name</ControlLabel>
 			        	<FormControl
+			        		name="firstName"
 			        		type="text"
 			        		placeholder="Enter your First Name"
 			        		value={this.state.firstName || ''}
@@ -54,8 +131,12 @@ export default  class Register extends React.Component{
 			        			})
 			        		}
 			        	/>
+			        <HelpBlock>{this.getClasses('firstName')}</HelpBlock>
+			        </FormGroup>
+			        <FormGroup  validationState={this.getClasses('lastName')}>
 			        	<ControlLabel>Last Name</ControlLabel>
 			        	<FormControl
+			        		name="lastName"
 			        		type="text"
 			        		placeholder="Enter your Last Name"
 			        		value={this.state.lastName || ''}
@@ -63,9 +144,14 @@ export default  class Register extends React.Component{
 			        				lastName:e.target.value
 			        			})
 			        		}
+			        		
 			        	/>
+			         <HelpBlock>{this.getClasses('lastName')}</HelpBlock>
+			        </FormGroup>
+			        <FormGroup  validationState={this.getClasses('email')}>
 			        	<ControlLabel>Email</ControlLabel>
 			        	<FormControl
+			        		name="email"
 			        		type="Email"
 			        		placeholder="Enter your Email"
 			        		value={this.state.email || ''}
@@ -74,8 +160,12 @@ export default  class Register extends React.Component{
 			        			})
 			        		}
 			        	/>
+			        <HelpBlock>{this.getClasses('email')}</HelpBlock>
+			        </FormGroup>
+			         <FormGroup  validationState={this.getClasses('password')}>
 			        	<ControlLabel>Password</ControlLabel>
 			        	<FormControl
+			        		name="password"
 			        		type="Password"
 			        		placeholder="Enter your Password"
 			        		value={this.state.password || ''}
@@ -85,8 +175,10 @@ export default  class Register extends React.Component{
 			        		}
 			        	/>
 			        	<FormControl.Feedback />
+			        	<HelpBlock>{this.getClasses('password')}</HelpBlock>
+			        	</FormGroup>
 			        	<ControlLabel>Sex</ControlLabel>
-			        	<FormGroup >
+			        	<FormGroup validationState={this.getClasses('gender')} >
 					      <Radio  inline name="gender" value="Male"
 					      	onChange={(e)=>this.setState({
 			        				gender:e.target.value
@@ -95,8 +187,6 @@ export default  class Register extends React.Component{
 					      >
 					        Male
 					      </Radio>
-					      {' '}
-					      {''}
 					      <Radio inline name="gender" value="Female"
 					      	onChange={(e)=>this.setState({
 			        				gender:e.target.value
@@ -105,8 +195,7 @@ export default  class Register extends React.Component{
 					      >
 					        Female
 					      </Radio>
-					      {' '}
-					      
+					      <HelpBlock>{this.getClasses('gender')}</HelpBlock>
 					    </FormGroup>
 					    <ControlLabel>Movie</ControlLabel>
 					    <FormGroup >
@@ -141,8 +230,10 @@ export default  class Register extends React.Component{
 					      </Checkbox>
 					      
 					    </FormGroup>
+					    <FormGroup validationState={this.getClasses('food')} >
 					    <ControlLabel>Favorite food</ControlLabel>
 					      <FormControl componentClass="select" placeholder="Select"
+					      		name="food"
 					      		value={this.state.food || ''}
 					      		onChange={(e)=>this.setState({
 			        				food:e.target.value
@@ -154,16 +245,20 @@ export default  class Register extends React.Component{
 					        <option  value="mango">mango</option>
 					        <option  value="banana">banana</option>
 					      </FormControl>
+					      <HelpBlock>{this.getClasses('food')}</HelpBlock>
+					    </FormGroup>
 					     <div style={btnstyle}>
 					     <ButtonGroup>
 				         	<Button bsStyle="primary" type="submit">Submit</Button>
 				         	<Button bsStyle="info" type="button" onClick={this.goToApp}>Cancel</Button>
 				         </ButtonGroup>
 				         </div>
-			    </from>
+			    </form>
 		    </Well>
 	    </div>
 		
 	);
 	}
 }
+
+export default validation(strategy)(Register);
